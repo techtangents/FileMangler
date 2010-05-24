642a5a74bd9ba9a21b6485b8954e61c8486e6e66
techtangents <ddoctor@samplesurgeon.com>
2010-05-24 21:53:51 +1000

    facading

--- src/main/java/com/techtangents/filemangler/api/DefaultFileMangler.java
+++ src/main/java/com/techtangents/filemangler/api/DefaultFileMangler.java
@@ -1,18 +1,22 @@
 package com.techtangents.filemangler.api;
 
+import com.techtangents.filemangler.api.bits.DefaultReaderReader;
 import com.techtangents.filemangler.api.bits.DefaultResourceReader;
 import com.techtangents.filemangler.api.bits.DefaultWholeFileReader;
 import com.techtangents.filemangler.api.bits.DefaultWholeFileWriter;
+import com.techtangents.filemangler.api.bits.ReaderReader;
 import com.techtangents.filemangler.api.bits.ResourceReader;
 import com.techtangents.filemangler.api.bits.WholeFileReader;
 import com.techtangents.filemangler.api.bits.WholeFileWriter;
 
 import java.io.File;
+import java.io.Reader;
 
 public class DefaultFileMangler implements FileMangler {
     private final WholeFileReader wholeFileReader = new DefaultWholeFileReader();
     private final WholeFileWriter wholeFileWriter = new DefaultWholeFileWriter();
     private final ResourceReader resourceReader = new DefaultResourceReader();
+    private final ReaderReader readerReader = new DefaultReaderReader();
 
     public String read(File f) {
         return wholeFileReader.read(f);
@@ -24,5 +28,9 @@ public class DefaultFileMangler implements FileMangler {
 
     public String read(Class clazz, String filename) {
         return resourceReader.read(clazz, filename);
+    }
+
+    public String read(Reader reader) {
+        return readerReader.read(reader);
     }
 }
--- src/main/java/com/techtangents/filemangler/api/FileMangler.java
+++ src/main/java/com/techtangents/filemangler/api/FileMangler.java
@@ -1,8 +1,9 @@
 package com.techtangents.filemangler.api;
 
+import com.techtangents.filemangler.api.bits.ReaderReader;
 import com.techtangents.filemangler.api.bits.ResourceReader;
 import com.techtangents.filemangler.api.bits.WholeFileReader;
 import com.techtangents.filemangler.api.bits.WholeFileWriter;
 
-public interface FileMangler extends WholeFileReader, WholeFileWriter, ResourceReader {
+public interface FileMangler extends WholeFileReader, WholeFileWriter, ResourceReader, ReaderReader {
 }
